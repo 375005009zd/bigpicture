f22524f559d841452b6f525cc5c735a6edd7ca35
375005009zd <375005009zd@github.com>
2019-03-27 14:07:11 +0800

    图片放大镜完成

--- index.js
+++ index.js
@@ -1,66 +1,57 @@
 
-  	var imgBox = document.getElementById('imgBox');
-    var imgBoxW = imgBox.clientWidth;
-    var imgBoxH = imgBox.clientHeight;
+  	var bigBox = document.getElementById('bigBox');
+    var bigBoxW = bigBox.clientWidth;
+    var bigBoxH = bigBox.clientHeight;
+    var bigImg = document.getElementById('bigImg');
+    var smallBox = document.getElementById('smallBox');
+    var smallBoxW = smallBox.clientWidth;
+    var smallBoxH = smallBox.clientHeight;
   	var magnifierMark = document.getElementById('magnifierMark');
-  	var orginMarkW = "";
-  	var orginMarkH = "";
-  	var markHalfW = "";
-  	var markHalfH = "";
+  	var  orginMarkW = parseInt(magnifierMark.style.width);
+    var  orginMarkH = parseInt(magnifierMark.style.height);
+    var  markHalfW = orginMarkW/2;
+    var markHalfH = orginMarkH/2;
 
   	//鼠标进入图片框时，显示放大区域和放大遮罩层
-  	imgBox.addEventListener('mouseenter',function(event){
+  	smallBox.addEventListener('mouseover',function(event){
   		var e = event || window.event;
       magnifierMark.style.display='block';
-      orginMarkW = document.querySelector("#magnifierMark").clientWidth;
-      orginMarkH = document.querySelector("#magnifierMark").clientHeight;
-      markHalfW = orginMarkW/2;
-      markHalfH = orginMarkH/2;
-
-  		if(e.offsetX<=markHalfW){
-  		    	magnifierMark.style.left = '0px';
-  		}else if(imgBoxW - e.offsetX <= markHalfW){
-            magnifierMark.style.left = (imgBoxW - orginMarkW) + 'px';
-  		}else {
-            magnifierMark.style.left = (e.offsetX - markHalfW) + 'px';
-
-      }
-
-      if(e.offsetY<=markHalfH){
-            magnifierMark.style.top = '0px';
-      }else if(imgBoxH- e.offsetY <= markHalfH) {
-            magnifierMark.style.top = (imgBoxH - orginMarkH) + 'px';
-      }else {
-           magnifierMark.style.top = (e.offsetY - markHalfH) + 'px';
-      }
-      console.log("mouseenter");
+      bigBox.style.display = 'block';
   	});
     
 
-  	imgBox.addEventListener('mouseleave',function(){
+  	smallBox.addEventListener('mouseout',function(event){
   		var e = event || window.event;
   		 magnifierMark.style.display='none';
+       bigBox.style.display='none';
   	});
 
-   imgBox.addEventListener('mouseover',function(event){
-           var e = event || window.event;
+   smallBox.addEventListener('mousemove',function(event){
+      var e = event || window.event;
+      var rateX = bigBoxW/orginMarkW;
+      var rateY = bigBoxH/orginMarkH;
 
       if(e.offsetX<=markHalfW){
             magnifierMark.style.left = '0px';
-      }else if(imgBoxW - e.offsetX <= markHalfW){
-            magnifierMark.style.left = (imgBoxW - orginMarkW) + 'px';
+            bigImg.style.left = '0px';
+      }else if(smallBoxW - e.offsetX <= markHalfW){
+            magnifierMark.style.left = (smallBoxW - orginMarkW) + 'px';
+            bigImg.style.left = -(smallBoxW - orginMarkW)*rateX + 'px';
       }else {
             magnifierMark.style.left = (e.offsetX - markHalfW) + 'px';
-
+             bigImg.style.left = -(e.offsetX - markHalfW)*rateX + 'px';
       }
 
       if(e.offsetY<=markHalfH){
             magnifierMark.style.top = '0px';
-      }else if(imgBoxH- e.offsetY <= markHalfH) {
-            magnifierMark.style.top = (imgBoxH - orginMarkH) + 'px';
+            bigImg.style.top = '0px';
+      }else if(smallBoxH- e.offsetY <= markHalfH) {
+            magnifierMark.style.top = (smallBoxH - orginMarkH) + 'px';
+             bigImg.style.top = -(smallBoxH - orginMarkH)*rateY + 'px';
       }else {
            magnifierMark.style.top = (e.offsetY - markHalfH) + 'px';
+           bigImg.style.top = -(e.offsetY - markHalfH)*rateY + 'px';
       }
-      console.log("mouseover");
     });
-    //@ sourceURL=index.js
+
+//@ sourceURL=index.js
